a74da46aa358c243b75f23a604ba06e70c9caf37
Zbigniew <w4lkh4rd@gmail.com>
2015-04-26 17:25:57 +0200

    Added intermediate restriction (Where) tests

--- LINQ.Exercises/LINQ.Exercises/Restriction.cs
+++ LINQ.Exercises/LINQ.Exercises/Restriction.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 
 namespace LINQ.Exercises
@@ -73,6 +74,70 @@ namespace LINQ.Exercises
             IEnumerable<int> result = ints;
 
             Assert.IsTrue(new[] { -3, -1, -4, -1, 5, -5 }.SequenceEqual(result));
+        }
+
+        #endregion
+
+        #region Intermediate - collection of strings
+
+        // this is test data, do not change it
+        private IEnumerable<string> strings = new Collection<string>() { "tiger", "lion", "swordfish", "penguin", "elephant", "shark" };
+
+        [TestMethod]
+        public void Where_string_length_is_shorter_than_5_letters_returns_1_string()
+        {
+            IEnumerable<string> result = strings;
+
+            Assert.AreEqual(1, result.Count());
+        }
+
+        [TestMethod]
+        public void Where_string_length_is_9_returns_expected_strings()
+        {
+            IEnumerable<string> result = strings;
+
+            Assert.AreEqual(1, result.Count());
+            Assert.IsTrue(new[] { "swordfish" }.SequenceEqual(result));
+        }
+
+        [TestMethod]
+        public void Where_string_starts_with_s()
+        {
+            IEnumerable<string> result = strings;
+
+            Assert.IsTrue(new[] { "swordfish", "shark" }.SequenceEqual(result));
+        }
+
+        [TestMethod]
+        public void Where_string_has_i_as_a_second_letter()
+        {
+            IEnumerable<string> result = strings;
+
+            Assert.IsTrue(new[] { "tiger", "lion" }.SequenceEqual(result));
+        }
+
+        [TestMethod]
+        public void Where_string_contains_e()
+        {
+            IEnumerable<string> result = strings;
+
+            Assert.IsTrue(new[] { "tiger", "penguin", "elephant" }.SequenceEqual(result));
+        }
+
+        [TestMethod]
+        public void Where_string_ends_with_uppercase_t()
+        {
+            IEnumerable<string> result = strings;
+
+            Assert.IsTrue(new[] { "elephant" }.SequenceEqual(result));
+        }
+
+        [TestMethod]
+        public void Where_substring_equals_to_io()
+        {
+            IEnumerable<string> result = strings;
+
+            Assert.IsTrue(new[] { "lion" }.SequenceEqual(result));
         }
 
         #endregion
