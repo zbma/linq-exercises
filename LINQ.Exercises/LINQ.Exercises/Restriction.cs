ecbe83f7cc684d8d324b5df4984077db66533689
Zbigniew <w4lkh4rd@gmail.com>
2015-05-02 17:47:48 +0200

    Moved test data to separate class.

--- LINQ.Exercises/LINQ.Exercises/Restriction.cs
+++ LINQ.Exercises/LINQ.Exercises/Restriction.cs
@@ -1,13 +1,12 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
 
 namespace LINQ.Exercises
 {
     /// <summary>
     /// Your task is to apply LINQ `Where` method, so all tests will be passed.
+    /// Make sure to preview test data located in TestData.cs file.
     /// Don't modify assertions or test data, all you need to do is to apply LINQ method so `result` variable will have expected value(s).
     /// </summary>
     [TestClass]
@@ -15,14 +14,11 @@ namespace LINQ.Exercises
     {
         #region Basic - array of integers
 
-        // this is test data, do not change it
-        private IEnumerable<int> ints = new[] { 1, -3, 1, -1, 2, -4, 3, -1, 5, -5 };
-
         [TestMethod]
         public void Where_n_is_greater_than_1_return_3_ints()
         {
             // First test is solved to show you how to use these exercises.
-            IEnumerable<int> result = ints.Where(n => n > 1);
+            IEnumerable<int> result = TestData.Integers.Where(n => n > 1);
 
             Assert.AreEqual(3, result.Count());
         }
@@ -30,7 +26,7 @@ namespace LINQ.Exercises
         [TestMethod]
         public void Where_n_is_less_than_or_equal_to_0_returns_expected_ints()
         {
-            IEnumerable<int> result = ints;
+            IEnumerable<int> result = TestData.Integers;
 
             Assert.AreEqual(5, result.Count());
             Assert.IsTrue(new[] { -3, -1, -4, -1, -5 }.SequenceEqual(result));
@@ -39,7 +35,7 @@ namespace LINQ.Exercises
         [TestMethod]
         public void Where_n_multiplied_by_2_is_greater_than_5()
         {
-            IEnumerable<int> result = ints;
+            IEnumerable<int> result = TestData.Integers;
 
             Assert.IsTrue(new[] { 3, 5 }.SequenceEqual(result));
         }
@@ -47,7 +43,7 @@ namespace LINQ.Exercises
         [TestMethod]
         public void Where_n_is_even()
         {
-            IEnumerable<int> result = ints;
+            IEnumerable<int> result = TestData.Integers;
 
             Assert.IsTrue(new[] { 2, -4 }.SequenceEqual(result));
         }
@@ -55,7 +51,7 @@ namespace LINQ.Exercises
         [TestMethod]
         public void Where_index_of_n_is_odd()
         {
-            IEnumerable<int> result = ints;
+            IEnumerable<int> result = TestData.Integers;
 
             Assert.IsTrue(new[] { 1, 1, 2, 3, 5 }.SequenceEqual(result));
         }
@@ -63,7 +59,7 @@ namespace LINQ.Exercises
         [TestMethod]
         public void Where_n_is_even_and_n_is_less_than_0()
         {
-            IEnumerable<int> result = ints;
+            IEnumerable<int> result = TestData.Integers;
 
             Assert.IsTrue(new[] { -4 }.SequenceEqual(result));
         }
@@ -72,7 +68,7 @@ namespace LINQ.Exercises
         public void Where_n_quare_minus_2_times_n_is_greater_than_n()
         {
             // n * n - 2 * n
-            IEnumerable<int> result = ints;
+            IEnumerable<int> result = TestData.Integers;
 
             Assert.IsTrue(new[] { -3, -1, -4, -1, 5, -5 }.SequenceEqual(result));
         }
@@ -81,13 +77,11 @@ namespace LINQ.Exercises
 
         #region Intermediate - collection of strings
 
-        // this is test data, do not change it
-        private IEnumerable<string> strings = new Collection<string>() { "tiger", "lion", "swordfish", "penguin", "elephant", "shark" };
 
         [TestMethod]
         public void Where_string_length_is_shorter_than_5_letters_returns_1_string()
         {
-            IEnumerable<string> result = strings;
+            IEnumerable<string> result = TestData.Strings;
 
             Assert.AreEqual(1, result.Count());
         }
@@ -95,7 +89,7 @@ namespace LINQ.Exercises
         [TestMethod]
         public void Where_string_length_is_9_returns_expected_strings()
         {
-            IEnumerable<string> result = strings;
+            IEnumerable<string> result = TestData.Strings;
 
             Assert.AreEqual(1, result.Count());
             Assert.IsTrue(new[] { "swordfish" }.SequenceEqual(result));
@@ -104,7 +98,7 @@ namespace LINQ.Exercises
         [TestMethod]
         public void Where_string_starts_with_s()
         {
-            IEnumerable<string> result = strings;
+            IEnumerable<string> result = TestData.Strings;
 
             Assert.IsTrue(new[] { "swordfish", "shark" }.SequenceEqual(result));
         }
@@ -112,7 +106,7 @@ namespace LINQ.Exercises
         [TestMethod]
         public void Where_string_has_i_as_a_second_letter()
         {
-            IEnumerable<string> result = strings;
+            IEnumerable<string> result = TestData.Strings;
 
             Assert.IsTrue(new[] { "tiger", "lion" }.SequenceEqual(result));
         }
@@ -120,7 +114,7 @@ namespace LINQ.Exercises
         [TestMethod]
         public void Where_string_contains_e()
         {
-            IEnumerable<string> result = strings;
+            IEnumerable<string> result = TestData.Strings;
 
             Assert.IsTrue(new[] { "tiger", "penguin", "elephant" }.SequenceEqual(result));
         }
@@ -128,7 +122,7 @@ namespace LINQ.Exercises
         [TestMethod]
         public void Where_string_ends_with_uppercase_t()
         {
-            IEnumerable<string> result = strings;
+            IEnumerable<string> result = TestData.Strings;
 
             Assert.IsTrue(new[] { "elephant" }.SequenceEqual(result));
         }
@@ -136,7 +130,7 @@ namespace LINQ.Exercises
         [TestMethod]
         public void Where_substring_equals_to_io()
         {
-            IEnumerable<string> result = strings;
+            IEnumerable<string> result = TestData.Strings;
 
             Assert.IsTrue(new[] { "lion" }.SequenceEqual(result));
         }
@@ -145,77 +139,52 @@ namespace LINQ.Exercises
 
         #region Advanced - list of persons
 
-        private class Person
-        {
-            public string FirstName { get; set; }
-
-            public string LastName { get; set; }
-
-            public DateTime Born { get; set; }
-
-            public Person(string firstname, string lastname, DateTime born)
-            {
-                this.FirstName = firstname;
-                this.LastName = lastname;
-                this.Born = born;
-            }
-        }
-
-        // this is test data, do not change it
-        private IList<Person> people = new List<Person>
-        {
-            new Person("Jack", "Tuck", new DateTime(1990, 3, 12)),
-            new Person("Jean", "Gean", new DateTime(1950, 12, 1)),
-            new Person("Jill", "Lill", new DateTime(2001, 5, 21)),
-            new Person("Jimmy", "Jilly", new DateTime(1974, 9, 16)),
-        };
-
         [TestMethod]
         public void Where_person_firstname_and_lastname_starts_with_same_letter()
         {
-            IEnumerable<Person> result = people;
+            IEnumerable<TestData.Person> result = TestData.People;
 
-            Assert.IsTrue(new[] { people[3] }.SequenceEqual(result));
+            Assert.IsTrue(new[] { TestData.People[3] }.SequenceEqual(result));
         }
 
         [TestMethod]
         public void Where_person_was_born_before_1990()
         {
-            IEnumerable<Person> result = people;
+            IEnumerable<TestData.Person> result = TestData.People;
 
-            Assert.IsTrue(new[] { people[1], people[3] }.SequenceEqual(result));
+            Assert.IsTrue(new[] { TestData.People[1], TestData.People[3] }.SequenceEqual(result));
         }
 
         [TestMethod]
         public void Where_person_was_born_on_day_with_even_number()
         {
-            IEnumerable<Person> result = people;
+            IEnumerable<TestData.Person> result = TestData.People;
 
-            Assert.IsTrue(new[] { people[0], people[3] }.SequenceEqual(result));
+            Assert.IsTrue(new[] { TestData.People[0], TestData.People[3] }.SequenceEqual(result));
         }
 
         [TestMethod]
         public void Where_person_was_born_on_monday_21st()
         {
-            IEnumerable<Person> result = people;
+            IEnumerable<TestData.Person> result = TestData.People;
 
-            Assert.IsTrue(new[] { people[2] }.SequenceEqual(result));
+            Assert.IsTrue(new[] { TestData.People[2] }.SequenceEqual(result));
         }
 
         [TestMethod]
         public void Where_person_had_18_years_or_more_in_2000()
         {
-            IEnumerable<Person> result = people;
+            IEnumerable<TestData.Person> result = TestData.People;
 
-            Assert.IsTrue(new[] { people[1], people[3] }.SequenceEqual(result));
+            Assert.IsTrue(new[] { TestData.People[1], TestData.People[3] }.SequenceEqual(result));
         }
 
         [TestMethod]
         public void Where_person_lastname_contains_ll_and_sum_of_year_month_day_is_greater_than_2000()
         {
-            IEnumerable<Person> result = people;
+            IEnumerable<TestData.Person> result = TestData.People;
 
-            Assert.IsTrue(new[] { people[2] }.SequenceEqual(result));
+            Assert.IsTrue(new[] { TestData.People[2] }.SequenceEqual(result));
         }
 
         #endregion
---
+++ LINQ.Exercises/LINQ.Exercises/TestData.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LINQ.Exercises
+{
+    /// <summary>
+    /// Data used for tests.
+    /// Don't change it
+    /// </summary>
+    internal static class TestData
+    {
+        internal static IEnumerable<int> Integers
+        {
+            get
+            {
+                return new[] { 1, -3, 1, -1, 2, -4, 3, -1, 5, -5 };
+            }
+        }
+
+        internal static ICollection<string> Strings
+        {
+            get
+            {
+                return new[] { "tiger", "lion", "swordfish", "penguin", "elephant", "shark" };
+            }
+        }
+
+        internal static IList<Person> People
+        {
+            get
+            {
+                return new[]
+                {
+                    new Person("Jack", "Tuck", new DateTime(1990, 3, 12)),
+                    new Person("Jean", "Gean", new DateTime(1950, 12, 1)),
+                    new Person("Jill", "Lill", new DateTime(2001, 5, 21)),
+                    new Person("Jimmy", "Jilly", new DateTime(1974, 9, 16)),
+                };
+            }
+        }
+
+        internal class Person
+        {
+            public string FirstName { get; set; }
+
+            public string LastName { get; set; }
+
+            public DateTime Born { get; set; }
+
+            public Person(string firstname, string lastname, DateTime born)
+            {
+                this.FirstName = firstname;
+                this.LastName = lastname;
+                this.Born = born;
+            }
+        }
+    }
+}
