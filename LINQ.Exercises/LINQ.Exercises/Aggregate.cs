0411df214733804cf48dc903810b1488911ad046
BKSpurgeon <benkoshy@hotmail.com>
2016-12-29 18:58:58 +1100

    add answer section for all the problems for student reference

--- LINQ.Exercises/LINQ.Exercises/Aggregate.cs
+++ LINQ.Exercises/LINQ.Exercises/Aggregate.cs
@@ -16,6 +16,10 @@ namespace LINQ.Exercises
         public void Count_all_numbers()
         {
             // First test is solved to show you how to use these exercises.
+
+            // This is the data used:
+            // int[] Numbers = new[] { 1, -3, 1, -1, 2, -4, 3, -1, 5, -5 };
+
             int result = TestData.Numbers.Count();
 
             Assert.AreEqual(10, result);
@@ -24,7 +28,14 @@ namespace LINQ.Exercises
         [TestMethod]
         public void Count_all_occurences_of_1()
         {
-            int result = TestData.Numbers.First();
+            // This is the data used:
+            // int[] Numbers = new[] { 1, -3, 1, -1, 2, -4, 3, -1, 5, -5 };
+
+            int result = TestData.Numbers.Count();
+
+            #region Answer
+            // int result = TestData.Numbers.Where(x => x ==1).Count(); 
+            #endregion
 
             Assert.AreEqual(2, result);
         }
@@ -32,8 +43,15 @@ namespace LINQ.Exercises
         [TestMethod]
         public void Count_all_animals_having_character_count_equal_to_5()
         {
+            // This is the data used:
+            // int[] Numbers = new[] { 1, -3, 1, -1, 2, -4, 3, -1, 5, -5 };
+
             // hint: use nested count
             int result = TestData.Animals.Count();
+
+            #region answer
+            // int result = TestData.Animals.Where( animalString => animalString.Count() == 5).Count(); 
+            #endregion
 
             Assert.AreEqual(2, result);
         }
@@ -41,7 +59,14 @@ namespace LINQ.Exercises
         [TestMethod]
         public void Sum_all_numbers()
         {
+            // This is the data used:
+            // int[] Numbers = new[] { 1, -3, 1, -1, 2, -4, 3, -1, 5, -5 };
+
             int result = TestData.Numbers.Count();
+
+            #region answers
+            // int result = TestData.Numbers.Sum(); 
+            #endregion
 
             Assert.AreEqual(-2, result);
         }
@@ -49,7 +74,14 @@ namespace LINQ.Exercises
         [TestMethod]
         public void Sum_all_characters_in_animal_names()
         {
+            // This is the data used:
+            // int[] Numbers = new[] { 1, -3, 1, -1, 2, -4, 3, -1, 5, -5 };
+
             int result = TestData.Animals.Count();
+
+            #region answer
+            // int result = TestData.Animals.Sum(animalString => animalString.Count());            
+            #endregion
 
             Assert.AreEqual(38, result);
         }
@@ -57,7 +89,14 @@ namespace LINQ.Exercises
         [TestMethod]
         public void Sum_all_birth_years()
         {
+            // This is the data used:
+            // int[] Numbers = new[] { 1, -3, 1, -1, 2, -4, 3, -1, 5, -5 };
+
             int result = TestData.People.Count();
+
+            #region answer
+            //int result = TestData.People.Sum(person => person.Born.Year);            
+            #endregion
 
             Assert.AreEqual(7915, result);
         }
@@ -65,7 +104,14 @@ namespace LINQ.Exercises
         [TestMethod]
         public void Find_minimum_number()
         {
+            // This is the data used:
+            // int[] Numbers = new[] { 1, -3, 1, -1, 2, -4, 3, -1, 5, -5 };
+
             int result = TestData.Numbers.Count();
+
+            #region answer
+            //int result = TestData.Numbers.Min();            
+            #endregion
 
             Assert.AreEqual(-5, result);
         }
@@ -73,7 +119,14 @@ namespace LINQ.Exercises
         [TestMethod]
         public void Find_length_of_shortest_animal_name()
         {
+            // This is the data used:
+            // int[] Numbers = new[] { 1, -3, 1, -1, 2, -4, 3, -1, 5, -5 };
+
             int result = TestData.Animals.Count();
+
+            #region MyRegion
+            //    int result = TestData.Animals.Min(animalString => animalString.Count());            
+            #endregion
 
             Assert.AreEqual(4, result);
         }
@@ -81,7 +134,14 @@ namespace LINQ.Exercises
         [TestMethod]
         public void Find_earliest_birthday()
         {
+            // This is the data used:
+            // int[] Numbers = new[] { 1, -3, 1, -1, 2, -4, 3, -1, 5, -5 };
+
             DateTime result = TestData.People.First().Born;
+
+            #region answer
+            // DateTime result = TestData.People.Select(person => person.Born).Min();            
+            #endregion
 
             Assert.AreEqual(new DateTime(1950, 12, 1), result);
         }
@@ -89,7 +149,14 @@ namespace LINQ.Exercises
         [TestMethod]
         public void Find_maximum_number()
         {
+            // This is the data used:
+            // int[] Numbers = new[] { 1, -3, 1, -1, 2, -4, 3, -1, 5, -5 };
+
             int result = TestData.Numbers.Count();
+
+            #region answer
+            // int result = TestData.Numbers.Max(); 
+            #endregion
 
             Assert.AreEqual(5, result);
         }
@@ -97,7 +164,14 @@ namespace LINQ.Exercises
         [TestMethod]
         public void Find_length_of_longest_animal_name()
         {
+            // This is the data used:
+            // int[] Numbers = new[] { 1, -3, 1, -1, 2, -4, 3, -1, 5, -5 };
+
             int result = TestData.Animals.Count();
+
+            #region answer
+            // int result = TestData.Animals.Max(x => x.Count()); 
+            #endregion
 
             Assert.AreEqual(9, result);
         }
@@ -105,7 +179,14 @@ namespace LINQ.Exercises
         [TestMethod]
         public void Find_latest_birthday()
         {
+            // This is the data used:
+            // int[] Numbers = new[] { 1, -3, 1, -1, 2, -4, 3, -1, 5, -5 };
+
             DateTime result = TestData.People.First().Born;
+
+            #region answer
+                // DateTime result = TestData.People.Max(person => person.Born); 
+            #endregion
 
             Assert.AreEqual(new DateTime(2001, 5, 21), result);
         }
@@ -113,7 +194,14 @@ namespace LINQ.Exercises
         [TestMethod]
         public void Find_average_of_numbers()
         {
+            // This is the data used:
+            // int[] Numbers = new[] { 1, -3, 1, -1, 2, -4, 3, -1, 5, -5 };
+
             double result = TestData.Numbers.Count();
+
+            #region answer
+                // double result = TestData.Numbers.Average();            
+            #endregion
 
             Assert.AreEqual(-0.2, result);
         }
@@ -121,7 +209,14 @@ namespace LINQ.Exercises
         [TestMethod]
         public void Find_average_of_birth_years()
         {
+            // This is the data used:
+            // int[] Numbers = new[] { 1, -3, 1, -1, 2, -4, 3, -1, 5, -5 };
+
             double result = TestData.People.Count();
+
+            #region answer
+                // double result = TestData.People.Select(p => p.Born.Year).Average(); 
+            #endregion
 
             Assert.AreEqual(1978.75, result);
         }
@@ -142,6 +237,10 @@ namespace LINQ.Exercises
             // product is an result of multiplication
             int result = TestData.Numbers.Aggregate((product, nextValue) => 1);
 
+            #region answer
+                // int result = TestData.Numbers.Aggregate( (sum, nextNumber) => sum * nextNumber );            
+            #endregion
+
             Assert.AreEqual(-1800, result);
         }
 
@@ -155,6 +254,8 @@ namespace LINQ.Exercises
             // else add 5 to it
             // and add resulting number to your aggregate
             int result = TestData.People.Aggregate(0, (sum, nextValue) => 1);
+
+            // Left as an exercise for the student
 
             Assert.AreEqual(296, result);
         }
