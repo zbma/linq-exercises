9521ed4fa5e605fe8540e4498707b8bb70bbb595
BKSpurgeon <benkoshy@hotmail.com>
2016-12-31 02:22:58 +1100

    add separate answer section, remove answers from code, grammar edit of some questions

--- LINQ.Exercises/LINQ.Exercises/Partition.cs
+++ LINQ.Exercises/LINQ.Exercises/Partition.cs
@@ -10,8 +10,8 @@ namespace LINQ.Exercises
     /// <summary>
     /// Look here for a primer:
     /// http://code.msdn.microsoft.com/LINQ-Partitioning-Operators-c68aaccc
-    /// hint: Use TakeWhile, TakeWhile - indexed, Skip, SkipWhile - indexed 
-    /// Look it up on Google and MSDN before checking the answers
+    /// HINTS: Use TakeWhile, TakeWhile - indexed: TakeWhile( (element, index) => etc ), Skip, SkipWhile - indexed  e.g. SkipWhile((element, index) => etc )
+    /// Please refer to the MSDN documentation if you have any trouble.
     /// Make sure to preview test data located in TestData.cs file.
     /// Don't modify assertions or test data, all you need to do is to apply LINQ method so `result` variable will have expected value(s).
     /// </summary>
@@ -35,13 +35,9 @@ namespace LINQ.Exercises
          public void GetFirstTwoNumbers_returns_2_ints()
          {           
              // please complete the below so the test passes:
-             IEnumerable<int> result = TestData.PartioningNumbers;
+             // Get the first two numbers in the array
 
-             #region answer
-
-             //IEnumerable<int> result = TestData.PartioningNumbers.Take(2);
-             
-             #endregion
+             IEnumerable<int> result = TestData.PartioningNumbers;          
 
              Assert.IsTrue(result.SequenceEqual(new[] { 5, 4 }));
          }         
@@ -49,14 +45,10 @@ namespace LINQ.Exercises
          [TestMethod]
          public void IgnoreFirstFourNumbers_returns_4_ints()
          {
+             // return everything in the array but the first four numbers:
+
              // please complete the below so the test passes:
-             IEnumerable<int> result = TestData.PartioningNumbers;
-
-             #region answer
-
-             //IEnumerable<int> result = TestData.PartioningNumbers.Skip(4);
-
-             #endregion            
+             IEnumerable<int> result = TestData.PartioningNumbers;                        
 
              Assert.IsTrue(result.SequenceEqual(new[] { 9, 8, 6, 7, 2, 0 }));
          }
@@ -65,69 +57,56 @@ namespace LINQ.Exercises
          public void Enumerate_Till_You_Get_A_Number_NotLessThanSix_returns_4_ints()
          {
              // This is tricky. Please read the below very carefully:
-             // This sample uses TakeWhile to return elements starting from the beginning of the array until a number is hit that is not less than 6.
+             // return elements starting from the beginning of the array until a number is hit that is not less than 6.
             
              // please complete the below so the test passes:
-              IEnumerable<int> result = TestData.PartioningNumbers;
+              IEnumerable<int> result = TestData.PartioningNumbers;             
 
-             #region answer
-             //IEnumerable<int> result = TestData.PartioningNumbers.TakeWhile(n => n < 6);
-             #endregion
-
-             Assert.IsTrue(result.SequenceEqual(new[] { 5,4,1,3 }));
+             Assert.IsTrue(result.SequenceEqual(new[] { 5,4,1,3 }));         
          }
 
          [TestMethod]
          public void Enumerate_Till_A_Number_Hit_Which_is_less_than_its_own_array_position_returns_2_ints()
          {
              // This is tricky. Please read the below very carefully:
-             // This sample uses TakeWhile to return elements starting from the beginning of the 
+             // return elements starting from the beginning of the 
              // array until a number is hit that is less than its position in the array.
 
              // please complete the below so the test passes:
-             IEnumerable<int> result = TestData.PartioningNumbers;
-
-             #region answer
-                // IEnumerable<int> result = TestData.PartioningNumbers.TakeWhile((n, indexNo) => !(n < indexNo)); 
-                // OR
-                // var result = numbers.TakeWhile((n, index) => n >= index); 
-             #endregion
+             IEnumerable<int> result = TestData.PartioningNumbers;           
 
              Assert.IsTrue(result.SequenceEqual(new[] { 5, 4 }));
+
+             #region Hint
+             //  Use: TakeWhile             
+             #endregion
          }
 
          [TestMethod]
          public void GetElementsOfArrayStartingFromTheFirstElementDivisibleByThree_Return7ints()
          {             
-             // Use SkipWhile to get the elements of the array starting from the first element divisible by 3.
+             // get the elements of the array starting from the first element divisible by 3.
              
              // please complete the below so the test passes:
              IEnumerable<int> result = TestData.PartioningNumbers;
 
-             #region answer
-              //IEnumerable<int> result = TestData.PartioningNumbers.SkipWhile((n) => (n % 3 != 0));
-             #endregion
-
-              Assert.IsTrue(result.SequenceEqual(new[] { 3, 9, 8, 6, 7, 2, 0 }));
+             Assert.IsTrue(result.SequenceEqual(new[] { 3, 9, 8, 6, 7, 2, 0 }));
          }
 
         [TestMethod]
          public void GetElementsStartingFromFirstElementLessThanItsPosition_Return8ints()
          {
-             // Use SkipWhile to get the elements of the array starting from the first element less than its position.
+             // get the elements of the array starting from the first element less than its position.
 
              // please complete the below so the test passes:
              IEnumerable<int> result = TestData.PartioningNumbers;
 
-             #region answer
-             // IEnumerable<int> result = TestData.PartioningNumbers.SkipWhile( (n, index) => n >= index) ;
-             #endregion
-
              Assert.IsTrue(result.SequenceEqual(new[] { 1, 3, 9, 8, 6, 7, 2, 0 }));
          }
-
-         //// Nested examples TBC
-
+      
+         // Want more questions? Want harder questions?
+         // Your valuable input is required.
+         // Why not contribute to this repo? YOU, yes you, can make a big difference!
 
     }
 }
